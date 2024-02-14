44b61807db60619fc2a41fed339b5ffcdeb008a1
b21antbr <b21antbr@student.his.se>
2024-02-14 11:43:23 +0100

    first commit

---
+++ app/src/main/java/com/example/lowdemand_xml/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lowdemand_xml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var spinner: Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        spinner = findViewById(R.id.spinner)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.dropdown_menu,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
+
+        val showDropdownButton: Button = findViewById(R.id.button1)
+        showDropdownButton.setOnClickListener {
+            spinner.performClick()
+        }
+    }
+}
