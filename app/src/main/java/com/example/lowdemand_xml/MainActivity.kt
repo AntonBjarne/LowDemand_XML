1349c5e5ea4789d3fd69ce92ac8993458135bb4f
b21antbr <b21antbr@student.his.se>
2024-02-16 15:55:11 +0100

    button re-added, dropdown working, application partially complete, some visual aspects needed

--- app/src/main/java/com/example/lowdemand_xml/MainActivity.kt
+++ app/src/main/java/com/example/lowdemand_xml/MainActivity.kt
@@ -3,32 +3,38 @@ package com.example.lowdemand_xml
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var dropdownButton: Button
     private lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        dropdownButton = findViewById(R.id.dropdown_button)
         spinner = findViewById(R.id.spinner)
 
-        val adapter = ArrayAdapter.createFromResource(
+        // Set an OnClickListener on the Button
+        dropdownButton.setOnClickListener {
+            // Programmatically trigger the Spinner's dropdown menu to show
+            spinner.performClick()
+        }
+
+        val items = resources.getStringArray(R.array.dropdown_menu).toMutableList()
+
+        val adapter = ArrayAdapter(
             this,
-            R.array.dropdown_menu,
-            android.R.layout.simple_spinner_item
+            android.R.layout.simple_spinner_item,
+            items
         )
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spinner.adapter = adapter
-
-        spinner.prompt = ""
-
-        spinner.setOnClickListener {
-            spinner.performClick()
-        }
     }
 }
